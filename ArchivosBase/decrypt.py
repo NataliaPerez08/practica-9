3fc802c4ed6f1ac09b4b7b6ba4be2d1f3134dc51
nataliaperez08 <natalia-perez08@ciencias.unam.mx>
2024-05-06 23:27:53 -0600

    Descifrar

--- ArchivosBase/decrypt.py
+++ ArchivosBase/decrypt.py
@@ -1,8 +1,11 @@
 # Modulo que se encarga de descifrar los datos de los pacientes
 
 from base64 import b64decode
-from Crypto.Cipher import AES
-    
+from Cryptodome.Cipher import AES
+from Cryptodome.Protocol.KDF import PBKDF2
+import MySQLdb, config, sys
+  
+
 def descifrar(diagnosis_ciphertext, treatment_ciphertext, diag_aes, treat_aes):
     # Decodificar el cifrado
     diagnosis_ciphertext = b64decode(diagnosis_ciphertext)
@@ -12,19 +15,49 @@ def descifrar(diagnosis_ciphertext, treatment_ciphertext, diag_aes, treat_aes):
     diagnosis = diag_aes.decrypt(diagnosis_ciphertext)
     treatment = treat_aes.decrypt(treatment_ciphertext)
     
+    # Decodificar los bytes a string
+    diagnosis = diagnosis.decode('utf-8')
+    treatment = treatment.decode('utf-8')
+
     return diagnosis, treatment
 
-def consultar_pacientes(cursor, diag_aes, treat_aes):
+
+def consultar_pacientes(password):
+    
+    mydb = MySQLdb.connect(user=config.user, password=config.password, database=config.dbname)
+    cursor = mydb.cursor()
+    
     select_query = """ SELECT * FROM expediente"""
     cursor.execute(select_query)
     records = cursor.fetchall()
     
     for row in records:
-        name = row[0]
-        diagnosis_ciphertext = row[1]
-        treatment_ciphertext = row[2]
+        name = row[1]
+        diagnosis_ciphertext = row[2]
+        treatment_ciphertext = row[3]
+        passwordSalt = row[4]
+        diagnosis_nonce = row[5]
+        treatment_nonce = row[6]
+        
+        print(f"Name: {name}\nDiagnosis: {diagnosis_ciphertext}\nTreatment: {treatment_ciphertext}\n")
+        
+        passwordSalt = b64decode(passwordSalt)
+        diagnosis_nonce = b64decode(diagnosis_nonce)
+        treatment_nonce = b64decode(treatment_nonce)
+
+        # Derivar la llave a partir de la contraseña y la salt
+        key = PBKDF2(password, passwordSalt, dkLen=32, count=1000000)
+        
+        # Crear los objetos AES para descifrar
+        diag_aes = AES.new(key,AES.MODE_GCM, nonce=diagnosis_nonce)
+        treat_aes = AES.new(key, AES.MODE_GCM, nonce=treatment_nonce)
         
         diagnosis, treatment = descifrar(diagnosis_ciphertext, treatment_ciphertext, diag_aes, treat_aes)
-        print(f"Name: {name}, Diagnosis: {diagnosis}, Treatment: {treatment}")
-        
+        print(f"Name: {name}\nDiagnosis: {diagnosis}\nTreatment: {treatment}\n")
     return records
+
+
+password = 'p7_aes'
+records = consultar_pacientes(password)
+
+    
